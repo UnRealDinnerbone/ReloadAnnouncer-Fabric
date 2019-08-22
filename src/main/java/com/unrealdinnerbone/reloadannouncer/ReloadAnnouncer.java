655f785cb1dd1824ae646ede156d52ee3578b93c
UnRealDinnerbone <UnRealDinnerbone@gmail.com>
2019-08-22 14:29:09 -0500

    Updated to 19w34a

--- src/main/java/com/unrealdinnerbone/reloadannouncer/ReloadAnnouncer.java
+++ src/main/java/com/unrealdinnerbone/reloadannouncer/ReloadAnnouncer.java
@@ -2,6 +2,7 @@ package com.unrealdinnerbone.reloadannouncer;
 
 import com.unrealdinnerbone.reloadannouncer.toast.ReloadToast;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.SharedConstants;
 import net.minecraft.server.PlayerManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,16 +13,17 @@ public class ReloadAnnouncer implements ClientModInitializer {
 
     private static long lastStartTime = 0;
     public static void onReloadStart(PlayerManager playerManager) {
-        ReloadToast.sendToat("Reloading!", "");
+        ReloadToast.sendToast("Reloading!", "");
         lastStartTime = System.currentTimeMillis();
     }
 
     public static void onReloadStop(PlayerManager playerManager) {
-        ReloadToast.sendToat("Reloaded!", "Reloaded in " + ((System.currentTimeMillis() - lastStartTime) / 1000) + "s") ;
+        ReloadToast.sendToast("Reloaded!", "Reloaded in " + ((System.currentTimeMillis() - lastStartTime) / 1000) + "s") ;
     }
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("[Reload Announcer Client] Loading!");
+        SharedConstants.isDevelopment = true;
     }
 }
--- src/main/java/com/unrealdinnerbone/reloadannouncer/toast/ReloadToast.java
+++ src/main/java/com/unrealdinnerbone/reloadannouncer/toast/ReloadToast.java
@@ -3,16 +3,17 @@ package com.unrealdinnerbone.reloadannouncer.toast;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.toast.ToastManager;
-import net.minecraft.text.StringTextComponent;
+import net.minecraft.text.LiteralText;
 
 public class ReloadToast extends SystemToast {
 
+    private static final ToastManager TOAST_MANAGER = MinecraftClient.getInstance().getToastManager();
 
     private ReloadToast(String title, String subTitle) {
-        super(Type.TUTORIAL_HINT, new StringTextComponent(title), new StringTextComponent(subTitle));
+        super(Type.TUTORIAL_HINT, new LiteralText(title), new LiteralText(subTitle));
     }
 
-    public static void sendToat(String title, String subTitle) {
+    public static void sendToast(String title, String subTitle) {
         ToastManager toastManager = MinecraftClient.getInstance().getToastManager();
         ReloadToast reloadToast = new ReloadToast(title, subTitle);
         toastManager.add(reloadToast);
