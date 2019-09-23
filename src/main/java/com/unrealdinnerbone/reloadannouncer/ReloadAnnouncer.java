874d720a671209ee7438bad55a2b6894554887e3
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2019-09-23 15:50:41 -0500

    Ok now this is the last version change i Swear

--- src/main/java/com/unrealdinnerbone/reloadannouncer/ReloadAnnouncer.java
+++ src/main/java/com/unrealdinnerbone/reloadannouncer/ReloadAnnouncer.java
@@ -2,7 +2,6 @@ package com.unrealdinnerbone.reloadannouncer;
 
 import com.unrealdinnerbone.reloadannouncer.toast.ReloadToast;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.SharedConstants;
 import net.minecraft.server.PlayerManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
